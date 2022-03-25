c68d3d0441107586921c7124be182e566046c313
Olayinka Oginni <olayinkaoginni2016@gmail.com>
2022-03-25 21:04:03 +0100

    flex-bug fixed

--- src/components/Service.js
+++ src/components/Service.js
@@ -7,8 +7,57 @@ export default function Service() {
   return (
     <div
       id='service'
-      className='h-full py-6 flex flex-col md:flex-row lg:flex-row items-center justify-evenly bg-gray-300'
+      className=' h-full py-40  flex flex-col md:flex-row gap-4 flex-wrap items-center justify-evenly bg-gray-300 '
     >
+      <div className='menu-card '>
+        <img src={ImageThree} alt='columbia-roast' className='img' />
+        <div className='py-4 text-center'>
+          <h5>House-blend</h5>
+          <p>Brazil, Congo, Portugal</p>
+          <span>$3</span>
+        </div>
+      </div>
+      <div className='menu-card '>
+        <img src={ImageOne} alt='columbia-roast' className='img' />
+        <div className='py-4 text-center'>
+          <h5>columbia roast</h5>
+          <p>Brazil, Congo, Portugal</p>
+          <span>$3</span>
+        </div>
+      </div>
+      <div className='menu-card '>
+        <img src={ImageThree} alt='columbia-roast' className='img' />
+        <div className='py-4 text-center'>
+          <h5>House-blend</h5>
+          <p>Brazil, Congo, Portugal</p>
+          <span>$3</span>
+        </div>
+      </div>
+      <div className='menu-card '>
+        <img src={ImageTwo} alt='french-roast' className='img' />
+        <div className='py-4 text-center'>
+          <h5>French roast</h5>
+          <p>Brazil, Congo, Portugal</p>
+          <span>$3</span>
+        </div>
+      </div>
+      <div className='menu-card '>
+        <img src={ImageThree} alt='columbia-roast' className='img' />
+        <div className='py-4 text-center'>
+          <h5>House-blend</h5>
+          <p>Brazil, Congo, Portugal</p>
+          <span>$3</span>
+        </div>
+      </div>
+
+      <div className='menu-card '>
+        <img src={ImageTwo} alt='french-roast' className='img' />
+        <div className='py-4 text-center'>
+          <h5>French roast</h5>
+          <p>Brazil, Congo, Portugal</p>
+          <span>$3</span>
+        </div>
+      </div>
       <div className='menu-card '>
         <img src={ImageOne} alt='columbia-roast' className='img' />
         <div className='py-4 text-center'>
@@ -26,9 +75,9 @@ export default function Service() {
         </div>
       </div>
       <div className='menu-card '>
-        <img src={ImageThree} alt='columbia-roast' className='img' />
+        <img src={ImageTwo} alt='french-roast' className='img' />
         <div className='py-4 text-center'>
-          <h5>House-blend</h5>
+          <h5>French roast</h5>
           <p>Brazil, Congo, Portugal</p>
           <span>$3</span>
         </div>
