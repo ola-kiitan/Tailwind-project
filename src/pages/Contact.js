1bd91a100d4715ee59843e04940250c4ff8126ae
Olayinka Oginni <olayinkaoginni2016@gmail.com>
2022-03-25 17:42:58 +0100

    contact-form

---
+++ src/pages/Contact.js
@@ -0,0 +1,40 @@
+import React from 'react'
+
+export default function Contact() {
+  return (
+    <div
+      className=' contact items-center h-full p-2 mt-2 shadow-md'
+      id='contact'
+    >
+      <div className='contact items-center bg-gray-100 shadow'>
+        <h1 className='text-2xl my-2 '>Contact-Us</h1>
+        <form action='' className='contact p-6 '>
+          <label htmlFor='name'>Name</label>
+          <input
+            type='text'
+            id='name'
+            className=' bg-slate-200 outline-none mb-2 '
+          />
+          <label htmlFor='email'>Email</label>
+          <input
+            type='text'
+            id='email'
+            className=' bg-slate-200 outline-none mb-2'
+          />
+          <label htmlFor='message'>Message</label>
+          <textarea
+            className=' bg-slate-200  outline-none '
+            id='message'
+            name='message'
+            rows='4'
+            cols='50'
+            placeholder='message'
+          ></textarea>
+          <button className=' bg-slate-500 text-white py-4 hover:bg-slate-400 hover:text-black'>
+            Submit
+          </button>
+        </form>
+      </div>
+    </div>
+  )
+}
--- src/pages/Home.js
+++ src/pages/Home.js
@@ -1,11 +1,13 @@
 import React from 'react'
 import Service from '../components/Service'
 import Hero from '../components/Hero'
+import Contact from './Contact'
 export default function Home() {
   return (
     <>
       <Hero />
       <Service />
+      <Contact />
     </>
   )
 }
